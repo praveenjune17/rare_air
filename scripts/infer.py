b48b74c25a1640d32b4cf3a8575e82581b2f21c8
Praveenkumar <praveen1050208@gmail.com>
2019-12-08 18:14:53 +0530

    Update infer.py

--- scripts/infer.py
+++ scripts/infer.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import sys
+sys.path.insert(0, '/content/rare_air/scripts')
+
 import tensorflow as tf
 tf.random.set_seed(100)
 import time
@@ -13,6 +16,17 @@ from beam_search import beam_search
 from preprocess import infer_data_from_df
 from creates import log
 
+transformer = Transformer(
+                          num_layers=config.num_layers, 
+                          d_model=config.d_model, 
+                          num_heads=config.num_heads, 
+                          dff=config.dff, 
+                          input_vocab_size=config.input_vocab_size, 
+                          target_vocab_size=config.target_vocab_size, 
+                          rate=h_parms.dropout_rate
+                          )
+generator   = Generator()
+
 def restore_chkpt(checkpoint_path):
     ckpt = tf.train.Checkpoint(
                                transformer=transformer,
@@ -21,7 +35,7 @@ def restore_chkpt(checkpoint_path):
                                )
     assert tf.train.latest_checkpoint(os.path.split(checkpoint_path)[0]), 'Incorrect checkpoint direcotry'
     ckpt.restore(checkpoint_path)
-    log.info(checkpoint_path, 'checkpoint restored for inference!!')
+    log.info(f'{checkpoint_path} restored')
 
 def beam_search_eval(document, beam_size):
   
@@ -68,31 +82,17 @@ def beam_search_eval(document, beam_size):
                      eos_id=[end]
                     )
 
-transformer = Transformer(
-                          num_layers=config.num_layers, 
-                          d_model=config.d_model, 
-                          num_heads=config.num_heads, 
-                          dff=config.dff, 
-                          input_vocab_size=config.input_vocab_size, 
-                          target_vocab_size=config.target_vocab_size, 
-                          rate=h_parms.dropout_rate
-                          )
-generator   = Generator()
+def run_inference(dataset, beam_sizes_to_try=h_parms.beam_sizes):
+    for beam_size in beam_sizes_to_try:
+      for (doc_id, (document, summary)) in enumerate(dataset, 1):
+        start_time = time.time()
+        # translated_output_temp[0] (batch, beam_size, summ_length+1)
+        translated_output_temp = beam_search_eval(document, beam_size)
+        log.info('Original summary: {}'.format(tokenizer_en.decode([j for j in tf.squeeze(summary) if j < tokenizer_en.vocab_size])))
+        log.info('Predicted summary: {}'.format(tokenizer_en.decode([j for j in tf.squeeze(translated_output_temp[0][:,0,:]) if j < tokenizer_en.vocab_size])))
+        log.info(f'time to process document {doc_id} : {time.time()-start_time}')
+      log.info(f'############ Beam size {beam_size} completed #########')
 
 #Restore the model's checkpoints
 restore_chkpt(file_path.infer_ckpt_path)
 infer_dataset = infer_data_from_df()
-
-def run_infer(beam_sizes_to_try = h_parms.beam_sizes):
-    for beam_size in beam_sizes_to_try:
-      for doc_id, document in enumerate(infer_dataset, 1):
-        start_time = time.time()
-        # translated_output_temp[0] (batch, beam_size, summ_length+1)
-        translated_output_temp = beam_search_eval(document, beam_size)
-        for top_sentence_ids in (translated_output_temp[0][:,0,:]):
-          log.info('Original document: {}'.format(sample_document))
-          log.info('Predicted summary: {}'.format(tokenizer_en.decode([j for j in top_sentence_ids if j < tokenizer_en.vocab_size])))
-        log.info(f'time to process document {doc_id} : {time.time()-start_time}')
-      log.info(f'############ Beam size {beam_size} completed #########')
-
-
