e15b72416063bccd1a3426c3455766ea974437fe
Praveenkumar <praveen1050208@gmail.com>
2019-12-16 14:16:08 +0530

    Update create_tokenizer.py

--- scripts/create_tokenizer.py
+++ scripts/create_tokenizer.py
@@ -16,7 +16,6 @@ def create_dataframe(path, num_examples):
 
 if os.path.exists(file_path.subword_vocab_path+'.subwords'):
   tokenizer_en = tfds.features.text.SubwordTextEncoder.load_from_file(file_path.subword_vocab_path)  
-  log.info('subword vocab file created')
 else:
   try:
     os.makedirs(os.path.split(file_path.subword_vocab_path)[0])
@@ -33,5 +32,5 @@ else:
     tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
                 (doc for doc, _ in zip(doc, summ)), target_vocab_size=2**13)
   tokenizer_en.save_to_file(file_path.subword_vocab_path)
-
+log.info('subword vocab file created')
 assert(tokenizer_en.vocab_size+2 == config.input_vocab_size== config.target_vocab_size), f' *vocab size in configuration script should be {tokenizer_en.vocab_size+2}'
