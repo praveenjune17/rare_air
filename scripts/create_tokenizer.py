e54050afd1755b55839a4ea23a064eba3c3c1eef
Praveenkumar <praveen1050208@gmail.com>
2019-12-17 17:45:35 +0530

    Update create_tokenizer.py

--- scripts/create_tokenizer.py
+++ scripts/create_tokenizer.py
@@ -15,25 +15,29 @@ def create_dataframe(path, num_examples):
     return (df["Document"].values, df["Summary"].values)
 
 if os.path.exists(file_path.subword_vocab_path+'.subwords'):
-  tokenizer_en = tfds.features.text.SubwordTextEncoder.load_from_file(file_path.subword_vocab_path)  
-elif os.path.split(file_path.subword_vocab_path)[1] + '.subwords' in os.listdir(file_path.G_drive_vocab_path):
-  new_vocab_path = os.path.join(file_path.G_drive_vocab_path, os.path.split(file_path.subword_vocab_path)[1])
-  tokenizer_en = tfds.features.text.SubwordTextEncoder.load_from_file(new_vocab_path)
+  tokenizer_en = tfds.features.text.SubwordTextEncoder.load_from_file(file_path.subword_vocab_path)
 else:
   try:
-    os.makedirs(os.path.split(file_path.subword_vocab_path)[0])
-  except FileExistsError:
-    pass
-  log.info('Vocab file not available so building it from the training set')
-  if config.use_tfds:
-    examples, metadata = tfds.load(config.tfds_name, with_info=True, as_supervised=True)
-    train_examples = examples['train']
-    tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+    if os.path.split(file_path.subword_vocab_path)[1] + '.subwords' in os.listdir(file_path.G_drive_vocab_path):
+      new_vocab_path = os.path.join(file_path.G_drive_vocab_path, os.path.split(file_path.subword_vocab_path)[1])
+      tokenizer_en = tfds.features.text.SubwordTextEncoder.load_from_file(new_vocab_path)
+  except FileNotFoundError:
+    log.info('Vocab file not available in G-drive)
+    try:
+      os.makedirs(os.path.split(file_path.subword_vocab_path)[0])
+    except FileExistsError:
+      pass
+    log.info('Building vocab file from the training set')
+    if config.use_tfds:
+      examples, metadata = tfds.load(config.tfds_name, with_info=True, as_supervised=True)
+      train_examples = examples['train']
+      tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
               (doc.numpy() for doc, _ in train_examples), target_vocab_size=2**13)
-  else:
-    doc, summ = create_dataframe(file_path.train_csv_path, None)
-    tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+    else:
+      doc, summ = create_dataframe(file_path.train_csv_path, None)
+      tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
                 (doc for doc, _ in zip(doc, summ)), target_vocab_size=2**13)
-  tokenizer_en.save_to_file(file_path.subword_vocab_path)
+    tokenizer_en.save_to_file(file_path.subword_vocab_path)
+             
 log.info('subword vocab file loaded')
 assert(tokenizer_en.vocab_size+2 == config.input_vocab_size== config.target_vocab_size), f' *vocab size in configuration script should be {tokenizer_en.vocab_size+2}'
